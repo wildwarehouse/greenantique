06fcd0e7cffaccf12b251fbc6749e7d6da76d854
Emory Merryman <emory.merryman+78c3ac06-99e8-4dbe-92fc-55f29bdfc7d5@gmail.com>
2017-02-25 12:32:11 +0000

    starting on some git flex scripts

---
+++ git-flex-milestone-create.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Copyright © (C) 2017 Emory Merryman <emory.merryman@gmail.com>
+#   This file is part of greenantique.
+#
+#   greenantique is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   greenantique is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with greenantique.  If not, see <http://www.gnu.org/licenses/>.
+
+curl --user "${GITHUB_USER_ID}:${GITHUB_TOKEN}" --data "{\"title\": \"${KEY_ID}\", \"key\": \"$(cat /home/user/.ssh/id_rsa.pub)\"}" https://api.github.com/repos/
